a97bbd3acc78b310d0b1d0067974709ea81f3c8d
HouBiHuang <r60179k09209@gmail.com>
2024-07-27 19:10:35 +0800

    Remove duplicated

--- OnlineDrinkShop/OnlineDrinkShop/OnlineDrinkShop/Models/UserSessionVm.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OnlineDrinkShop.Models
-{
-    public class UserSessionVm
-    {
-        public string UserName { get; set; } = string.Empty;
-        public string RoleName { get; set; } = string.Empty;
-    }
-}
